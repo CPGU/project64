d801c962dc13646cdf1970eead4c333f9d8f8310
ChrisPantelli <chrispantelli.1@gmail.com>
2018-02-26 15:28:44 +0000

    Fixed Merged Conflict

--- Project64/artist.js
+++ Project64/artist.js
@@ -12,12 +12,7 @@ function preload() {
 
     apiURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+searchQuery+"&api_key=50e22ce4bf1afc04593ed626f21223d7&format=json";
     result = loadJSON(apiURL);
-<<<<<<< HEAD
-    
-    
 
-=======
->>>>>>> ae3e696443da6ee7d4e7add967247200cc2eec46
 }
 
 function setup() {
@@ -25,7 +20,6 @@ function setup() {
 }
 
 function draw() {
-<<<<<<< HEAD
     if(result.error > 0) {
         window.location.replace("404.html");
     }
@@ -39,7 +33,5 @@ function draw() {
     artistListeners = select('#artistListeners');
     artistListeners.html(artistInfoListeners);
 
-=======
     artistNameElement.html(result.artist.name);
->>>>>>> ae3e696443da6ee7d4e7add967247200cc2eec46
 }
