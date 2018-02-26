5cc6820855e2265927dd468e8900ef37b5c53bb7
ChrisPantelli <chrispantelli.1@gmail.com>
2018-02-26 15:25:56 +0000

    Added Error Page & Stats

--- Project64/artist.js
+++ Project64/artist.js
@@ -13,6 +13,8 @@ function preload() {
 
     apiURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+searchQuery[1]+"&api_key=50e22ce4bf1afc04593ed626f21223d7&format=json";
     result = loadJSON(apiURL);
+    
+    
 
 }
 
@@ -22,8 +24,19 @@ function setup() {
 }
 
 function draw() {
+    if(result.error > 0) {
+        window.location.replace("404.html");
+    }
+
     var artistInfoPlayCount = result.artist.stats.playcount;
-    console.log(artistInfoPlayCount);
+    var artistInfoListeners = result.artist.stats.listeners;
+
+    artistPlays = select('#artistPlays');
+    artistPlays.html(artistInfoPlayCount);
+
+    artistListeners = select('#artistListeners');
+    artistListeners.html(artistInfoListeners);
+
 }
 
 //function artistInformation(data) {
